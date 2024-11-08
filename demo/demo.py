2433304c76622c9cd6f917d69a3753daa25694fe
luya1234 <durian.liu@barcoverse.com>
2024-11-08 09:26:57 +0800

    del print

--- demo/demo.py
+++ demo/demo.py
@@ -14,7 +14,6 @@ from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
 
-print("test01")
 from vision.fair.detectron2.demo.predictor import VisualizationDemo
 
 # constants
