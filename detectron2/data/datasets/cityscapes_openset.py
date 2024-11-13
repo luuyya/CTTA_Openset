1d5f5e394c39227564c7089cdf476a9548692a1c
luya1234 <durian.liu@barcoverse.com>
2024-11-13 16:32:08 +0800

    cityscapes_openset.py modify

--- detectron2/data/datasets/cityscapes_openset.py
+++ detectron2/data/datasets/cityscapes_openset.py
@@ -20,6 +20,7 @@ except ImportError:
     # OpenCV is an optional dependency at the moment
     pass
 
+#todo: 如何处理unknownid，需要理解数据在模型当中的运作模式，包括类别在其中的作用模式！！！！
 
 logger = logging.getLogger(__name__)
 
@@ -59,7 +60,7 @@ def _get_cityscapes_files(image_dir, gt_dir):
     return files
 
 
-def load_cityscapes_instances(image_dir, gt_dir, from_json=True, to_polygons=True):
+def load_cityscapes_instances_openset(image_dir, gt_dir, openset_setting=1, from_json=True, to_polygons=True, is_train=True):
     """
 
     Args:
@@ -85,7 +86,7 @@ def load_cityscapes_instances(image_dir, gt_dir, from_json=True, to_polygons=Tru
     pool = mp.Pool(processes=max(mp.cpu_count() // get_world_size() // 2, 4))
 
     ret = pool.map( #多了两个参数输入
-        functools.partial(_cityscapes_files_to_dict, from_json=from_json, to_polygons=to_polygons),
+        functools.partial(_cityscapes_files_to_dict, openset_setting=openset_setting, from_json=from_json, to_polygons=to_polygons, is_train=is_train),
         files,
     )
     logger.info("Loaded {} images from {}".format(len(ret), image_dir))
@@ -101,40 +102,7 @@ def load_cityscapes_instances(image_dir, gt_dir, from_json=True, to_polygons=Tru
     return ret
 
 
-def load_cityscapes_semantic(image_dir, gt_dir):
-    """
-    Args:
-        image_dir (str): path to the raw dataset. e.g., "~/cityscapes/leftImg8bit/train".
-        gt_dir (str): path to the raw annotations. e.g., "~/cityscapes/gtFine/train".
-
-    Returns:
-        list[dict]: a list of dict, each has "file_name" and
-            "sem_seg_file_name".
-    """
-    ret = []
-    # gt_dir is small and contain many small files. make sense to fetch to local first
-    gt_dir = PathManager.get_local_path(gt_dir)
-    for image_file, _, label_file, json_file in _get_cityscapes_files(image_dir, gt_dir):
-        label_file = label_file.replace("labelIds", "labelTrainIds")
-
-        with PathManager.open(json_file, "r") as f:
-            jsonobj = json.load(f)
-        ret.append(
-            {
-                "file_name": image_file,
-                "sem_seg_file_name": label_file,
-                "height": jsonobj["imgHeight"],
-                "width": jsonobj["imgWidth"],
-            }
-        )
-    assert len(ret), f"No images found in {image_dir}!"
-    assert PathManager.isfile(
-        ret[0]["sem_seg_file_name"]
-    ), "Please generate labelTrainIds.png with cityscapesscripts/preparation/createTrainIdLabelImgs.py"  # noqa
-    return ret
-
-
-def _cityscapes_files_to_dict(files, from_json, to_polygons):
+def _cityscapes_files_to_dict(files, openset_setting, from_json, to_polygons, is_train):
     """
     Parse cityscapes annotation files to a instance segmentation dataset dict.
 
@@ -187,11 +155,10 @@ def _cityscapes_files_to_dict(files, from_json, to_polygons):
             poly = Polygon(poly_coord).buffer(0.5, resolution=4)
 
             if not label.hasInstances or label.ignoreInEval:
-                # even if we won't store the polygon it still contributes to overlaps resolution
                 polygons_union = polygons_union.union(poly)
                 continue
 
-            # Take non-overlapping part of the polygon
+            # 去除上一个多边形的覆盖范围
             poly_wo_overlaps = poly.difference(polygons_union)
             if poly_wo_overlaps.is_empty:
                 continue
@@ -210,9 +177,6 @@ def _cityscapes_files_to_dict(files, from_json, to_polygons):
 
             poly_coord = []
             for poly_el in poly_list:
-                # COCO API can work only with exterior boundaries now, hence we store only them.
-                # TODO: store both exterior and interior boundaries once other parts of the
-                # codebase support holes in polygons.
                 poly_coord.append(list(chain(*poly_el.exterior.coords)))
             anno["segmentation"] = poly_coord
             (xmin, ymin, xmax, ymax) = poly_wo_overlaps.bounds
@@ -220,7 +184,18 @@ def _cityscapes_files_to_dict(files, from_json, to_polygons):
             anno["bbox"] = (xmin, ymin, xmax, ymax)
             anno["bbox_mode"] = BoxMode.XYXY_ABS
 
-            annos.append(anno)
+            BASE_CLASSES, NOVEL_CLASSES, ALL_CLASSES, CLASS_NAMES = get_openset_settings_cityscapes(openset_setting)
+
+            if is_train:
+                if label_name in BASE_CLASSES:
+                    annos.append(anno)
+            else:
+                if label_name in BASE_CLASSES:
+                    annos.append(anno)
+                else:
+                    anno['category_id']=label.id #todo:add unknown id https://blog.csdn.net/talender/article/details/103254073
+                    annos.append(anno)
+
     else:
         # See also the official annotation parsing scripts at
         # https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/evaluation/instances2dict.py  # noqa
@@ -274,58 +249,81 @@ def _cityscapes_files_to_dict(files, from_json, to_polygons):
     ret["annotations"] = annos
     return ret
 
+def get_openset_settings_cityscapes(setting):
 
-def main() -> None:
-    global logger, labels
-    """
-    Test the cityscapes dataset loader.
+    NMES = ['person', 'car', 'train', 'rider', 'truck', 'motorcycle', 'bicycle', 'bus']
+    UNK = ["unknown"]
 
-    Usage:
-        python -m detectron2.data.datasets.cityscapes \
-            cityscapes/leftImg8bit/train cityscapes/gtFine/train
-    """
-    import argparse
+    if setting == 1:  # different semantics
+        BASE_CLASSES = ['car', 'truck', 'bus']
+        NOVEL_CLASSES = ['person', 'motorcycle', 'train', 'bicycle', 'rider']
+    elif setting == 2:  # similar semantics
+        BASE_CLASSES = ['person', 'bicycle', 'bus']
+        NOVEL_CLASSES = ['car', 'truck', 'train', 'motorcycle', 'rider']
+    elif setting == 3:  # frequency down
+        BASE_CLASSES = ['person', 'car', 'rider']
+        NOVEL_CLASSES = ['bicycle', 'train', 'truck', 'motorcycle', 'bus']
+    elif setting == 4:  # frequency top
+        BASE_CLASSES = ['motorcycle', 'truck', 'bus']
+        NOVEL_CLASSES = ['person', 'train', 'car', 'bicycle', 'rider']
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("image_dir")
-    parser.add_argument("gt_dir")
-    parser.add_argument("--type", choices=["instance", "semantic"], default="instance")
-    args = parser.parse_args()
-    from cityscapesscripts.helpers.labels import labels
-    from detectron2.data.catalog import Metadata
-    from detectron2.utils.visualizer import Visualizer
+    ALL_CLASSES = tuple(BASE_CLASSES+NOVEL_CLASSES)
+    CLASS_NAMES = tuple(BASE_CLASSES+UNK)
 
-    logger = setup_logger(name=__name__)
-
-    dirname = "cityscapes-data-vis"
-    os.makedirs(dirname, exist_ok=True)
-
-    if args.type == "instance":
-        dicts = load_cityscapes_instances(
-            args.image_dir, args.gt_dir, from_json=True, to_polygons=True
-        )
-        logger.info("Done loading {} samples.".format(len(dicts)))
-
-        thing_classes = [k.name for k in labels if k.hasInstances and not k.ignoreInEval]
-        meta = Metadata().set(thing_classes=thing_classes)
-
-    else:
-        dicts = load_cityscapes_semantic(args.image_dir, args.gt_dir)
-        logger.info("Done loading {} samples.".format(len(dicts)))
-
-        stuff_classes = [k.name for k in labels if k.trainId != 255]
-        stuff_colors = [k.color for k in labels if k.trainId != 255]
-        meta = Metadata().set(stuff_classes=stuff_classes, stuff_colors=stuff_colors)
-
-    for d in dicts:
-        img = np.array(Image.open(PathManager.open(d["file_name"], "rb")))
-        visualizer = Visualizer(img, metadata=meta)
-        vis = visualizer.draw_dataset_dict(d)
-        # cv2.imshow("a", vis.get_image()[:, :, ::-1])
-        # cv2.waitKey()
-        fpath = os.path.join(dirname, os.path.basename(d["file_name"]))
-        vis.save(fpath)
+    return BASE_CLASSES, NOVEL_CLASSES, ALL_CLASSES, CLASS_NAMES
 
 
-if __name__ == "__main__":
-    main()  # pragma: no cover
+# def main() -> None:
+#     global logger, labels
+#     """
+#     Test the cityscapes dataset loader.
+#
+#     Usage:
+#         python -m detectron2.data.datasets.cityscapes \
+#             cityscapes/leftImg8bit/train cityscapes/gtFine/train
+#     """
+#     import argparse
+#
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("image_dir")
+#     parser.add_argument("gt_dir")
+#     parser.add_argument("--type", choices=["instance", "semantic"], default="instance")
+#     args = parser.parse_args()
+#     from cityscapesscripts.helpers.labels import labels
+#     from detectron2.data.catalog import Metadata
+#     from detectron2.utils.visualizer import Visualizer
+#
+#     logger = setup_logger(name=__name__)
+#
+#     dirname = "cityscapes-data-vis"
+#     os.makedirs(dirname, exist_ok=True)
+#
+#     if args.type == "instance":
+#         dicts = load_cityscapes_instances(
+#             args.image_dir, args.gt_dir, from_json=True, to_polygons=True
+#         )
+#         logger.info("Done loading {} samples.".format(len(dicts)))
+#
+#         thing_classes = [k.name for k in labels if k.hasInstances and not k.ignoreInEval]
+#         meta = Metadata().set(thing_classes=thing_classes)
+#
+#     else:
+#         dicts = load_cityscapes_semantic(args.image_dir, args.gt_dir)
+#         logger.info("Done loading {} samples.".format(len(dicts)))
+#
+#         stuff_classes = [k.name for k in labels if k.trainId != 255]
+#         stuff_colors = [k.color for k in labels if k.trainId != 255]
+#         meta = Metadata().set(stuff_classes=stuff_classes, stuff_colors=stuff_colors)
+#
+#     for d in dicts:
+#         img = np.array(Image.open(PathManager.open(d["file_name"], "rb")))
+#         visualizer = Visualizer(img, metadata=meta)
+#         vis = visualizer.draw_dataset_dict(d)
+#         # cv2.imshow("a", vis.get_image()[:, :, ::-1])
+#         # cv2.waitKey()
+#         fpath = os.path.join(dirname, os.path.basename(d["file_name"]))
+#         vis.save(fpath)
+#
+#
+# if __name__ == "__main__":
+#     main()  # pragma: no cover
